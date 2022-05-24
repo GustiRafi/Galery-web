d1a87c787d23873d7485104c2ac918a2b3ab15d6
GustiRafi <gustirafi49@gmail.com>
2022-05-24 12:25:52 +0700

    add file About.php

---
+++ app/controllers/About.php
@@ -0,0 +1,18 @@
+<?php 
+
+class About
+{
+    public function index()
+    {
+        echo "ini halaman about/index";
+    }
+
+    public function page()
+    {
+        echo "about/page";
+    }
+}
+
+
+
+?>

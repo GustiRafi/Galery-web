e63625ef8706d14afef7d9a687262b96f6f1b215
GustiRafi <gustirafi49@gmail.com>
2022-05-24 22:07:54 +0700

    add view to controller about

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,24 @@
 <?php 
 
-class About
+class About extends Controller
 {
-    public function index()
+    public function index($nama= 'Rafi', $pekerjaan ='Pelajar', $umur = 15)
     {
-        echo "ini halaman about/index";
+        $data["page"] = 'About';
+        $data["nama"] = $nama;
+        $data["pekerjaan"] = $pekerjaan;
+        $data["umur"] = $umur;
+        $this->view('components/header', $data);
+        $this->view('about/index' , $data);
+        $this->view('components/footer');
     }
 
     public function page()
     {
-        echo "about/page";
+        $data["page"] = 'Page';
+        $this->view('components/header', $data);
+        $this->view('about/page');
+        $this->view('components/footer');
     }
 }
 

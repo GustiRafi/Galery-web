44ed5d213b6af0528c2632fa1c1427019774030c
GustiRafi <gustirafi49@gmail.com>
2022-05-24 22:20:42 +0700

    make footer

---
+++ app/views/components/footer.php
@@ -0,0 +1,12 @@
+<script src="<?= BASEURL ?>/js/bootstrap.js"></script>
+<script src="<?= BASEURL ?>/js/jquery-3.6.0.min.js"></script>
+</body>
+
+<hr class="dropdown-divider">
+<footer>
+    <div class="container">
+        <p>copyright &copy 2022 by Rafi-Gusti</p>
+    </div>
+</footer>
+
+</html>
